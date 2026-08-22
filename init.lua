a9e897aa17a60fbd9b4685557a38902641c181f1
Iggor Uellendahl <iggorvalente@hotmail.com>
2026-08-22 18:55:42 +0200

    rocks enabled is false now

--- init.lua
+++ init.lua
@@ -945,6 +945,7 @@ require('lazy').setup({
     end,
   },
   checker = { enabled = false },
+  rocks = { enabled = false },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
